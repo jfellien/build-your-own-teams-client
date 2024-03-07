d551a834b79f6fd478aae0137de4c8ee212981ed
Janek Fellien <fellien@medialesson.de>
2024-03-07 12:42:55 +0100

    use my app registration

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     msalConfig: {
         auth: {
-            clientId: 'b5c2e510-4a17-4feb-b219-e55aa5b74144',
-            authority: 'https://login.microsoftonline.com/common'
+            clientId: '21e3ad9c-8e9b-45b9-8abd-0bd27d58c334',
+            authority: 'https://login.microsoftonline.com/3157d5b8-9db5-431c-98fb-fa2fdcedf861'
         }
     },
     apiConfig: {
