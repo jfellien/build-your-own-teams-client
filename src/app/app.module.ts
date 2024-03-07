650ed1bf70d91eb6f9b5b66acc9012f11c6e0f87
Janek Fellien <fellien@medialesson.de>
2024-03-07 10:49:58 +0100

    added two modules home and profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, ProfileComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
