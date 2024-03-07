91a98f0bd6469252af0b6d4c05587a5e2991357c
Janek Fellien <fellien@medialesson.de>
2024-03-07 12:15:07 +0100

    complete copy from MS, because generation did not work

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+type ProfileType = {
+  givenName?: string,
+  surname?: string,
+  userPrincipalName?: string,
+  id?: string
+};
 
 @Component({
   selector: 'app-profile',
-  standalone: true,
-  imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profile: ProfileType | undefined;
 
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.getProfile(environment.apiConfig.uri);
+  }
+
+  getProfile(url: string) {
+    this.http.get(url)
+      .subscribe(profile => {
+        this.profile = profile;
+      });
+  }
 }
